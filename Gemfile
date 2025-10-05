150a207ca0ed32d67561153773945b2ca7692f75
fuwa <fuwafuwa41.glr@gmail.com>
2025-10-05 22:32:47 +0900

    6-4までやった

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem "hamlit-rails"
 gem "omniauth"
 gem "omniauth-github"
 gem "omniauth-rails_csrf_protection"
+gem "rails-i18n", "~> 8.0.0"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  helper_method :logged_in?
+  before_action :authenticate
+  helper_method :logged_in?, :current_user
   allow_browser versions: :modern
   layout "application"
 
   private
 
   def logged_in?
-    !!session[:user_id]
+    !!current_user
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: "ログインしてください"
   end
 end
